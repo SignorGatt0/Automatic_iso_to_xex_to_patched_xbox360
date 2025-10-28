a5f349ecc6151b874a366be9e8bf43451e356e60
Arlo <arlottimarco17@gmail.com>
2025-10-28 21:10:29 +0000

    Add files via upload
    fixed some issue with the function that ask for directories path

--- terminal_interface.py
+++ terminal_interface.py
@@ -1,22 +1,35 @@
 from art import text2art
 from termcolor import colored
-from tkinter.filedialog import askdirectory
 
-# creating the program title
-Art = text2art("ARLO", font="sub-zero")
-print(colored(Art, "magenta"))
+import os
+from sys import platform
 
-Art = text2art("AUTO", font="sub-zero")
-print(colored(Art, "magenta"))
-
-Art = text2art("PATCHER", font="sub-zero")
-print(colored(Art, "magenta"))
+if platform == "linux":
+    clear = "clear"   
+elif platform == "win32":
+    clear = "cls"
+    
+os.system(clear)
 
 # setup for the start of the application
 iso_path = False
 extract_path = False
 ready = False
 reload = True
+
+def chose_folder() -> str:
+    """
+    Function that ask to the user a file path to use,
+    the function return the where the file is
+    """
+    test_exist = False
+    while not test_exist:
+        path = str(input("insert FULL directory path..."))
+        
+        path = path.replace('"','') #remove "" from the inserted path
+        test_exist = os.path.isdir(path) #check if the folder exist 
+    return path
+
 
 def first_option() -> str:
     global iso_path
@@ -28,7 +41,9 @@ def first_option() -> str:
 
     # promt for selecting a folder for the ISOs
     input_folder:str = chose_folder()
+    input_folder:str = colored(input_folder,'yellow') #color the path for better highlighting
 
+    print(f"CURRENT ISO DIRECTORY SET TO | {input_folder}")
     # update the interface to display that a folder has been chosen
     iso_path = True
 
@@ -44,8 +59,11 @@ def second_option()-> str:
     """
 
     # promt for selecting a folder for the ISOs
-    extract_folder: str = chose_folder()
+    extract_folder:str = chose_folder()
 
+    extract_folder = colored(extract_folder,'yellow') #color the path for better highlighting
+
+    print(f"CURRENT EXTRACTION DIRECTORY SET TO | {extract_folder}")
     # update the interface to display that a folder has been chosen
     extract_path = True
 
@@ -58,25 +76,29 @@ def third_option():
         # start
         pass
     elif not iso_path or not extract_path:
-        # stop the start 
+        # stop the start from the missing requirement 
         print(colored('ERROR THE ISO OR THE OUTPUT DIRECTORY HAS NOT BEEN CHOSED','red'))
-        return
-    
-
-def chose_folder() -> str:
-    """
-    Function that ask to the user a file path to use,
-    the function return the where the file is
-    """
-
-    path:str = askdirectory(title="select a directory")
-    return path
+        x = input("")
 
 # application main loop
 while reload:
 
+    # creating the program title
+    Art = text2art("ARLO", font="sub-zero")
+    print(colored(Art, "magenta"))
+
+    Art = text2art("AUTO", font="sub-zero")
+    print(colored(Art, "magenta"))
+
+    Art = text2art("PATCHER", font="sub-zero")
+    print(colored(Art, "magenta"))
 
     # update the interface
+    
+        # the program is ready for the third option
+    if iso_path and extract_path:
+        ready = True
+
     if iso_path:
         iso_path_output = "DONE"
     else:
@@ -88,36 +110,33 @@ while reload:
         extract_path_output = "X"
 
     if ready:
-        ready_output = colored("READY TO START", 'light_blue')
+        ready_output = colored("READY TO START", 'magenta')
     else:
         ready_output = colored("NOT READY", 'red')
 
-    # the program is ready for the third option
-    if iso_path and extract_path:
-        ready = True
+    
 
+    
     # the options creation
     # 1
     print(colored(f"1 |{iso_path_output}| select the folder of the ISOs", "green"))
     # 2
-    print(
-        colored(
-            f"2 |{extract_path_output}| select the folder to put the extracted fies",
-            "white",
-        )
-    )
+    
+    print(colored(f"2 |{extract_path_output}| select the folder to put the extracted fies","white",))
+
     # 3
     print(f"3 |{ready_output}| START AUTOMATIC PROCESS")
-
-    choice = int(input("..."))
-
-    input_folder = first_option()
+    
+    choice = int(input("... "))
     match choice:
         case 1:
             input_folder = first_option()
+            
         case 2:
             extract_folder = second_option()
+            
         case 3:
             third_option()
+    os.system(clear)
 
 
